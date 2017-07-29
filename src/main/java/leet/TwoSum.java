97b691bd32bee73be1c2ae6f8e98d2b963424418
jasonFuBooking <jason.fu@booking.com>
2017-07-29 14:08:00 +0800

    two sum

--- src/main/java/leet/TwoSum.java
+++ src/main/java/leet/TwoSum.java
@@ -1,8 +1,24 @@
 package leet;
 
+import java.util.*;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            set.add(i);
+        }
 
+        for (int i = 0; i < nums.length; ++i) {
+            int currentValue = nums[i];
+            if (set.contains(target - currentValue)) {
+                for (int j = i + 1; j < nums.length; ++j) {
+                    if (nums[j] == (target - currentValue)) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+        }
         return null;
     }
 }
--- src/test/java/leet/TwoSumTest.java
+++ src/test/java/leet/TwoSumTest.java
@@ -4,12 +4,34 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TwoSumTest {
+    private TwoSum twoSum = new TwoSum();
     @Test
     public void twoSum() {
-        TwoSum twoSum = new TwoSum();
         int[] a = new int[]{2, 7, 11, 15};
         int target = 9;
         Assert.assertArrayEquals(new int[]{0, 1}, twoSum.twoSum(a, target));
     }
+
+    @Test
+    public void twoSum2() {
+        int[] a = new int[]{2, 7, 11, 15};
+        int target = 10;
+        Assert.assertArrayEquals(null, twoSum.twoSum(a, target));
+    }
+
+    @Test
+    public void twoSum3() {
+        int[] a = new int[]{};
+        int target = 10;
+        Assert.assertArrayEquals(null, twoSum.twoSum(a, target));
+    }
+
+    @Test
+    public void twoSum4() {
+        int[] a = new int[]{1, 2};
+        int target = 10;
+        Assert.assertArrayEquals(null, twoSum.twoSum(a, target));
+    }
+
 
 }
