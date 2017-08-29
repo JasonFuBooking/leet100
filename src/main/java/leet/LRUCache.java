65f73261a9b47e98e44944de3b3c85beb3b51519
Jason Fu <jason.fu@booking.com>
2017-08-29 11:30:18 +0800

    LRU Cache

---
+++ src/main/java/leet/LRUCache.java
@@ -0,0 +1,127 @@
+package leet;
+
+/*
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+ */
+
+import java.util.Hashtable;
+
+/*
+这个题我失误点在于一看到O(1)就慌了。。。
+其实hashmap也是近似O(1)呀亲，
+这里的点就在于用一个hashmap来保存实际的键值对
+然后维护一个单独的双向链表，一旦某个元素被访问了，就移动到链表的头，这样链表尾就是应该被LRU的元素
+ */
+public class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode pre;
+        DLinkedNode post;
+    }
+
+    /**
+     * Always add the new node right after head;
+     */
+    private void addNode(DLinkedNode node){
+        node.pre = head;
+        node.post = head.post;
+
+        head.post.pre = node;
+        head.post = node;
+    }
+
+    /**
+     * Remove an existing node from the linked list.
+     */
+    private void removeNode(DLinkedNode node){
+        DLinkedNode pre = node.pre;
+        DLinkedNode post = node.post;
+
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    /**
+     * Move certain node in between to the head.
+     */
+    private void moveToHead(DLinkedNode node){
+        this.removeNode(node);
+        this.addNode(node);
+    }
+
+    // pop the current tail.
+    private DLinkedNode popTail(){
+        DLinkedNode res = tail.pre;
+        this.removeNode(res);
+        return res;
+    }
+
+    private Hashtable<Integer, DLinkedNode>
+            cache = new Hashtable<Integer, DLinkedNode>();
+    private int count;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.count = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        head.pre = null;
+
+        tail = new DLinkedNode();
+        tail.post = null;
+
+        head.post = tail;
+        tail.pre = head;
+    }
+
+    public int get(int key) {
+
+        DLinkedNode node = cache.get(key);
+        if(node == null){
+            return -1; // should raise exception here.
+        }
+
+        // move the accessed node to the head;
+        this.moveToHead(node);
+
+        return node.value;
+    }
+
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if(node == null){
+
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            this.cache.put(key, newNode);
+            this.addNode(newNode);
+
+            ++count;
+
+            if(count > capacity){
+                // pop the tail
+                DLinkedNode tail = this.popTail();
+                this.cache.remove(tail.key);
+                --count;
+            }
+        }else{
+            // update the value.
+            node.value = value;
+            this.moveToHead(node);
+        }
+
+    }
+}
---
+++ src/test/java/leet/LRUCacheTest.java
@@ -0,0 +1,16 @@
+package leet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LRUCacheTest {
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void put() throws Exception {
+    }
+
+}
