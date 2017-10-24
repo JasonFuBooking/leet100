e4c19cdbd040bece3b9975105c7a9705bd14c17f
Jason Fu <jason.fu@booking.com>
2017-10-24 19:01:41 +0800

    538

--- src/main/java/leet/next/_538_Convert_BST_to_Greater_Tree.java
+++ src/main/java/leet/next/_538_Convert_BST_to_Greater_Tree.java
@@ -2,6 +2,9 @@ package leet.next;
 
 import util.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
 
@@ -17,30 +20,26 @@ import util.TreeNode;
    /   \
  20     13
  */
-public class _538_Convert_BST_to_Greater_Tree {
-    public TreeNode convertBST(TreeNode root) {
-        return null;
-    }
-}
 
 /*
-
-Idea: Reversely traverse the tree and keep a sum of all previously visited values. Because its a BST, values seen before are all greater than current node.val. That's what we want according to the problem.
-
-public class Solution {
-    int sum = 0;
-
+这一题的思路相对简单。
+中序遍历BST是一个有序序列。
+然后再倒序依次累加即可。
+ */
+public class _538_Convert_BST_to_Greater_Tree {
     public TreeNode convertBST(TreeNode root) {
-        if (root == null) return null;
-
-        convertBST(root.right);
-
-        root.val += sum;
-        sum = root.val;
-
-        convertBST(root.left);
-
+        List<TreeNode> list = new ArrayList<>();
+        traverse(root, list);
+        for (int i = list.size() - 1; i > 0; --i) {
+            list.get(i - 1).val += list.get(i).val;
+        }
         return root;
     }
+
+    private void traverse(TreeNode node, List<TreeNode> list) {
+        if (node == null) return;
+        traverse(node.left, list);
+        list.add(node);
+        traverse(node.right, list);
+    }
 }
- */
