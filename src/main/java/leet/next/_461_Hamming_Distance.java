7e5a24896e86bdafe50ab37058bf4b7f4a93baf9
Jason Fu <jason.fu@booking.com>
2017-10-25 09:30:18 +0800

    461    Hamming Distance

--- src/main/java/leet/next/_461_Hamming_Distance.java
+++ src/main/java/leet/next/_461_Hamming_Distance.java
@@ -23,18 +23,6 @@ package leet.next;
  */
 public class _461_Hamming_Distance {
     public int hammingDistance(int x, int y) {
-        return 0;
-    }
-}
-
-
-/*
-
-What does come to your mind first when you see this sentence "corresponding bits are different"? Yes, XOR! Also, do not forget there is a decent function Java provided: Integer.bitCount() ~~~
-
-public class Solution {
-    public int hammingDistance(int x, int y) {
         return Integer.bitCount(x ^ y);
     }
 }
- */
