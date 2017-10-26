750846cdadb63d82f81a64a31c8550ba37da8cf6
Jason Fu <jason.fu@booking.com>
2017-10-26 14:18:42 +0800

    226. Invert Binary Tree

--- src/main/java/leet/next/_226_Invert_Binary_Tree.java
+++ src/main/java/leet/next/_226_Invert_Binary_Tree.java
@@ -21,8 +21,21 @@ import util.TreeNode;
  This problem was inspired by this original tweet by Max Howell:
  Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
  */
+
+/*
+这道题关键的关键就是要看明白翻转的过程可以分解成；
+1. 首先根节点的左右子树交换
+2. 左右子树的左右子树再分别交换即可
+3. 注意35和36行，左右子树单独拿出来先，要不后边两行的赋值就乱了
+典型的递归
+ */
 public class _226_Invert_Binary_Tree {
     public TreeNode invertTree(TreeNode root) {
-        return null;
+        if (root == null) return null;
+        TreeNode right = root.right;
+        TreeNode left = root.left;
+        root.right = invertTree(left);
+        root.left = invertTree(right);
+        return root;
     }
 }
---
+++ src/test/java/leet/next/_226_Invert_Binary_TreeTest.java
@@ -0,0 +1,18 @@
+package leet.next;
+
+import org.junit.Test;
+import util.TreeNode;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jafu on 10/26/17.
+ */
+public class _226_Invert_Binary_TreeTest {
+    @Test
+    public void invertTree() throws Exception {
+        _226_Invert_Binary_Tree test = new _226_Invert_Binary_Tree();
+        test.invertTree(TreeNode.createTree3());
+    }
+
+}
