35b3c61468e92e019d52c2c5c61911d59ca23244
Jason Fu <jason.fu@booking.com>
2017-10-27 18:05:55 +0800

    39. Combination Sum

--- src/main/java/leet/next/_039_CombinationSum.java
+++ src/main/java/leet/next/_039_CombinationSum.java
@@ -1,6 +1,6 @@
 package leet.next;
 
-import java.util.List;
+import java.util.*;
 
 /**
  Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
@@ -17,161 +17,40 @@ import java.util.List;
  [2, 2, 3]
  ]
  */
-public class _039_CombinationSum {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
-    }
-}
 
 /*
-This structure might apply to many other backtracking questions, but here I am just going to demonstrate Subsets, Permutations, and Combination Sum.
+因为同一元素可以重复，所以稍微麻烦点。
+思路就是借助循环和递归遍历所有情况
+注意50行是从0开始，用来遍历自身重复的情况，44行保证了不会无限递归。46行保存结果集，一定要记得53行出栈
 
-Subsets : https://leetcode.com/problems/subsets/
-
-public List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
-        tempList.remove(tempList.size() - 1);
-    }
-}
-Subsets II (contains duplicates) : https://leetcode.com/problems/subsets-ii/
-
-public List<List<Integer>> subsetsWithDup(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
-        tempList.remove(tempList.size() - 1);
-    }
-}
-Permutations : https://leetcode.com/problems/permutations/
-
-public List<List<Integer>> permute(int[] nums) {
-   List<List<Integer>> list = new ArrayList<>();
-   // Arrays.sort(nums); // not necessary
-   backtrack(list, new ArrayList<>(), nums);
-   return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-   if(tempList.size() == nums.length){
-      list.add(new ArrayList<>(tempList));
-   } else{
-      for(int i = 0; i < nums.length; i++){
-         if(tempList.contains(nums[i])) continue; // element already exists, skip
-         tempList.add(nums[i]);
-         backtrack(list, tempList, nums);
-         tempList.remove(tempList.size() - 1);
-      }
-   }
-}
-Permutations II (contains duplicates) : https://leetcode.com/problems/permutations-ii/
-
-public List<List<Integer>> permuteUnique(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
-    if(tempList.size() == nums.length){
-        list.add(new ArrayList<>(tempList));
-    } else{
-        for(int i = 0; i < nums.length; i++){
-            if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
-            used[i] = true;
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, used);
-            used[i] = false;
-            tempList.remove(tempList.size() - 1);
-        }
-    }
-}
-Combination Sum : https://leetcode.com/problems/combination-sum/
-
-public List<List<Integer>> combinationSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            tempList.remove(tempList.size() - 1);
-        }
-    }
-}
-Combination Sum II (can't reuse same element) : https://leetcode.com/problems/combination-sum-ii/
-
-public List<List<Integer>> combinationSum2(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{
-        for(int i = start; i < nums.length; i++){
-            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i + 1);
-            tempList.remove(tempList.size() - 1);
-        }
-    }
-}
-Palindrome Partitioning : https://leetcode.com/problems/palindrome-partitioning/
-
-public List<List<String>> partition(String s) {
-   List<List<String>> list = new ArrayList<>();
-   backtrack(list, new ArrayList<>(), s, 0);
-   return list;
-}
-
-public void backtrack(List<List<String>> list, List<String> tempList, String s, int start){
-   if(start == s.length())
-      list.add(new ArrayList<>(tempList));
-   else{
-      for(int i = start; i < s.length(); i++){
-         if(isPalindrome(s, start, i)){
-            tempList.add(s.substring(start, i + 1));
-            backtrack(list, tempList, s, i + 1);
-            tempList.remove(tempList.size() - 1);
-         }
-      }
-   }
-}
-
-public boolean isPalindrome(String s, int low, int high){
-   while(low < high)
-      if(s.charAt(low++) != s.charAt(high--)) return false;
-   return true;
-}
  */
+public class _039_CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        sum(candidates, target, result, new LinkedList<>());
+
+        Set<List<Integer>> set = new HashSet<>();
+        for (List<Integer> list : result) {
+            Collections.sort(list);
+            set.add(list);
+        }
+        result.clear();
+        result.addAll(set);
+        return result;
+    }
+
+    private void sum(int[] candidates, int remain, List<List<Integer>> result, List<Integer> list) {
+        if (remain < 0) return;
+        if (remain == 0) {
+            result.add(new LinkedList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < candidates.length && remain >= candidates[i]; ++i) {
+            list.add(candidates[i]);
+            sum(candidates, remain - candidates[i], result, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
---
+++ src/test/java/leet/next/_039_CombinationSumTest.java
@@ -0,0 +1,17 @@
+package leet.next;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jafu on 10/27/17.
+ */
+public class _039_CombinationSumTest {
+    @Test
+    public void combinationSum() throws Exception {
+        _039_CombinationSum test = new _039_CombinationSum();
+        test.combinationSum(new int[]{2, 3, 6, 7} ,7);
+    }
+
+}
