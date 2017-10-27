26207fbe1f3e7aa489f45a5fbcd00611e0f12778
Jason Fu <jason.fu@booking.com>
2017-10-27 15:53:09 +0800

    34. Search for a Range

---
+++ src/test/java/leet/next/_034_SearchForARangeTest.java
@@ -0,0 +1,17 @@
+package leet.next;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jafu on 10/27/17.
+ */
+public class _034_SearchForARangeTest {
+    @Test
+    public void searchRange() throws Exception {
+        _034_SearchForARange test = new _034_SearchForARange();
+        test.searchRange(new int[]{5,7,7,8,8,10}, 8);
+    }
+
+}
