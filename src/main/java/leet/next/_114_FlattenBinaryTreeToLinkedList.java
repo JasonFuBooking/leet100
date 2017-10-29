bfcb7c7e230c67fd4d32075ed8ba9ff99560d49e
jasonFuBooking <jason.fu@booking.com>
2017-10-29 22:52:39 +0800

    114. Flatten Binary Tree to Linked List

--- src/main/java/leet/next/_114_FlattenBinaryTreeToLinkedList.java
+++ src/main/java/leet/next/_114_FlattenBinaryTreeToLinkedList.java
@@ -2,6 +2,8 @@ package leet.next;
 
 import util.TreeNode;
 
+import java.util.*;
+
 /**
  Given a binary tree, flatten it to a linked list in-place.
 
@@ -30,28 +32,36 @@ import util.TreeNode;
  Hints:
  If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
  */
-public class _114_FlattenBinaryTreeToLinkedList {
-    public void flatten(TreeNode root) {
-
-    }
-}
 
 /*
-it is DFS so u need a stack. Dont forget to set the left child to null, or u'll get TLE. (tricky!)
+因为用了stack和list，所以执行时间变成了20ms。。。
+大家都在用递归的直接改
 
-   public void flatten(TreeNode root) {
-        if (root == null) return;
-        Stack<TreeNode> stk = new Stack<TreeNode>();
-        stk.push(root);
-        while (!stk.isEmpty()){
-            TreeNode curr = stk.pop();
-            if (curr.right!=null)
-                 stk.push(curr.right);
-            if (curr.left!=null)
-                 stk.push(curr.left);
-            if (!stk.isEmpty())
-                 curr.right = stk.peek();
-            curr.left = null;  // dont forget this!!
-        }
-    }
+这里用的是前序遍历的非递归，最后在统一修改指针的方法
+
  */
+public class _114_FlattenBinaryTreeToLinkedList {
+
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        List<TreeNode> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+           while (node != null) {
+               list.add(node);
+               stack.push(node);
+               node = node.left;
+           }
+           node = stack.pop();
+           node = node.right;
+        }
+
+        for (int i = 0; i < list.size() - 1; ++i) {
+            list.get(i).left = null;
+            list.get(i).right = list.get(i + 1);
+        }
+        list.get(list.size() - 1).left = null;
+        list.get(list.size() - 1).right = null;
+    }
+}
---
+++ src/test/java/leet/next/_114_FlattenBinaryTreeToLinkedListTest.java
@@ -0,0 +1,15 @@
+package leet.next;
+
+import org.junit.Test;
+import util.TreeNode;
+
+import static org.junit.Assert.*;
+
+public class _114_FlattenBinaryTreeToLinkedListTest {
+    @Test
+    public void flatten() throws Exception {
+        _114_FlattenBinaryTreeToLinkedList test = new _114_FlattenBinaryTreeToLinkedList();
+        test.flatten(TreeNode.createTree2());
+    }
+
+}
