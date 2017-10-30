b3f9d25829bd677f1227e5daee0363049d400ed9
jasonFuBooking <jason.fu@booking.com>
2017-10-31 00:13:54 +0800

    152. Maximum Product Subarray v1

--- src/main/java/leet/next/_152_MaximumProductSubarray.java
+++ src/main/java/leet/next/_152_MaximumProductSubarray.java
@@ -8,32 +8,18 @@ package leet.next;
  */
 public class _152_MaximumProductSubarray {
     public int maxProduct(int[] nums) {
-        return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        for (int i = 1; i < nums.length; ++i) {
+            int max = nums[i];
+            if (nums[i] == nums[i - 1] + 1) {
+                max = Math.max(max, nums[i] * dp[i - 1]);
+            } else {
+                max = Math.max(max, dp[i - 1]);
+            }
+            dp[i] = max;
+        }
+        return dp[nums.length - 1];
     }
 }
-
-
-/*
-int maxProduct(int A[], int n) {
-    // store the result that is the max we have found so far
-    int r = A[0];
-
-    // imax/imin stores the max/min product of
-    // subarray that ends with the current number A[i]
-    for (int i = 1, imax = r, imin = r; i < n; i++) {
-        // multiplied by a negative makes big number smaller, small number bigger
-        // so we redefine the extremums by swapping them
-        if (A[i] < 0)
-            swap(imax, imin);
-
-        // max/min product for the current number is either the current number itself
-        // or the max/min by the previous number times the current one
-        imax = max(A[i], imax * A[i]);
-        imin = min(A[i], imin * A[i]);
-
-        // the newly computed max value is a candidate for our global result
-        r = max(r, imax);
-    }
-    return r;
-}
- */
---
+++ src/test/java/leet/next/_152_MaximumProductSubarrayTest.java
@@ -0,0 +1,14 @@
+package leet.next;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class _152_MaximumProductSubarrayTest {
+    @Test
+    public void maxProduct() throws Exception {
+        _152_MaximumProductSubarray test = new _152_MaximumProductSubarray();
+        test.maxProduct(new int[] {2,3,-2,4});
+    }
+
+}
