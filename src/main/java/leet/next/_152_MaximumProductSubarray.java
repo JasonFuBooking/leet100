2ae105d1125b43565a6b27c19e51a6b3d8f8c30f
Jason Fu <jason.fu@booking.com>
2017-10-31 10:45:45 +0800

    152. Maximum Product Subarray

--- src/main/java/leet/next/_152_MaximumProductSubarray.java
+++ src/main/java/leet/next/_152_MaximumProductSubarray.java
@@ -6,20 +6,32 @@ package leet.next;
  For example, given the array [2,3,-2,4],
  the contiguous subarray [2,3] has the largest product = 6.
  */
+
+/*z
+这道题有两个点我失误了。
+1. 有负数的存在，所以要同时设置一个dp_min来保存最小值，防止负负得正
+2. 在递推的过程中只需要考虑
+    a. 当前值i是否加入i-1的最优解 dp_max[i - 1] * nums[i]
+    b. 当前值i乘以相反的极值（求dp_max 就乘以 dp_min[i - 1]）
+    c. 当前值本身
+ */
 public class _152_MaximumProductSubarray {
     public int maxProduct(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp_min = new int[nums.length];
+        int[] dp_max = new int[nums.length];
+        dp_min[0] = nums[0];
+        dp_max[0] = nums[0];
+        int result = nums[0];
 
         for (int i = 1; i < nums.length; ++i) {
-            int max = nums[i];
-            if (nums[i] == nums[i - 1] + 1) {
-                max = Math.max(max, nums[i] * dp[i - 1]);
-            } else {
-                max = Math.max(max, dp[i - 1]);
-            }
-            dp[i] = max;
+            dp_max[i] = Math.max(Math.max(dp_min[i - 1] * nums[i], dp_max[i - 1] * nums[i]), nums[i]);
+            dp_min[i] = Math.min(Math.min(dp_max[i - 1] * nums[i], dp_min[i - 1] * nums[i]), nums[i]);
+            result = Math.max(result, dp_max[i]);
         }
-        return dp[nums.length - 1];
+        return result;
     }
 }
--- src/test/java/leet/next/_152_MaximumProductSubarrayTest.java
+++ src/test/java/leet/next/_152_MaximumProductSubarrayTest.java
@@ -1,5 +1,6 @@
 package leet.next;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,7 +9,10 @@ public class _152_MaximumProductSubarrayTest {
     @Test
     public void maxProduct() throws Exception {
         _152_MaximumProductSubarray test = new _152_MaximumProductSubarray();
-        test.maxProduct(new int[] {2,3,-2,4});
+        Assert.assertEquals(test.maxProduct(new int[] {2, 3, -2, 4}), 6);
+        Assert.assertEquals(test.maxProduct(new int[] {-1, -1}), 1);
+        Assert.assertEquals(test.maxProduct(new int[] {-2, 3, -4}), 24);
+        Assert.assertEquals(test.maxProduct(new int[] {2, 3, 0, 4}), 6);
     }
 
 }
