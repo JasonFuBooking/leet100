4d632c91d7164427cc200c94454eeb67800dc0f7
jasonFuBooking <jason.fu@booking.com>
2017-10-31 21:48:47 +0800

    200. Number of Islands

--- src/main/java/leet/next/_200_Number_of_Islands.java
+++ src/main/java/leet/next/_200_Number_of_Islands.java
@@ -1,8 +1,6 @@
 package leet.next;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
@@ -27,41 +25,81 @@ import java.util.Map;
 
  Credits:
  Special thanks to @mithmatt for adding this problem and creating all test cases.
+ */
 
+/*
+我最开始的算法是逐行遍历，逐个看每一个节点的上下左右，把连在一起的归类，但是逐个看有个问题
+就是我的 testcase 1 这种情况， 会多算。
+
+第二次改是我注释掉的代码，其实我已经用了DFS了，只不过用了额外的set来保存遍历过的节点。
+
+最后一次就是现在的代码，参考答案之后。去掉了辅助存储，改为直接在数组上操作，
+凡是遍历过得统统改为0.
+因为一坐岛会在一次search中都会递归遍历完，所以44行下一次循环开始时，还剩下的1肯定就是剩下的岛屿的。
 
  */
 public class _200_Number_of_Islands {
     public int numIslands(char[][] grid) {
-        Map<String, Object> map = new HashMap<>();
+        int count = 0;
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
-                String current = i + "" + j;
-                if (j - 1 >= 0) {
-                    assignShip(map, i + "" + (j - 1), current);
-                }
-                if (j + 1 < grid[i].length) {
-                    assignShip(map, i + "" + (j + 1), current);
-                }
-                if (i - 1 > 0) {
-                    assignShip(map, (i - 1) + "" + j, current);
-                }
-                if (i + 1 < grid.length) {
-                    assignShip(map, (i + 1) + "" + j, current);
-                }
+                if (grid[i][j] == '0') continue;
+                search(grid, i, j);
+                ++count;
             }
         }
-        return map.values().size();
+        return count;
     }
 
-    private void assignShip(Map<String, Object> map, String target, String current) {
-        Object ship = map.get(target);
-        Object currentShip = map.get(current);
-        if (ship == null && currentShip == null) {
-            ship = new Object();
-            map.put(target, ship);
-        } else if (ship == null) {
-            ship = currentShip;
+    private void search(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length
+                || grid[i][j] == '0') {
+            return;
         }
-        map.put(current, ship);
+        grid[i][j] = '0';
+        search(grid, i, j - 1);
+        search(grid, i, j + 1);
+        search(grid, i - 1, j);
+        search(grid, i + 1, j);
     }
+
+//    public int numIslands(char[][] grid) {
+//        Map<String, Object> map = new HashMap<>();
+//        for (int i = 0; i < grid.length; ++i) {
+//            for (int j = 0; j < grid[i].length; ++j) {
+//                String current = i + "" + j;
+//                if (grid[i][j] == '0') continue;
+//                if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+//                    assignShip(map, i + "" + (j - 1), current);
+//                }
+//                if (j + 1 < grid[i].length && grid[i][j + 1] == '1') {
+//                    assignShip(map, i + "" + (j + 1), current);
+//                }
+//                if (i - 1 > 0 && grid[i - 1][j] == '1') {
+//                    assignShip(map, (i - 1) + "" + j, current);
+//                }
+//                if (i + 1 < grid.length && grid[i + 1][j] == '1') {
+//                    assignShip(map, (i + 1) + "" + j, current);
+//                }
+//                map.computeIfAbsent(current, k -> new Object());
+//            }
+//        }
+//        Set<Object> set = new HashSet<>();
+//        set.addAll(map.values());
+//        return set.size();
+//    }
+//
+//    private void assignShip(Map<String, Object> map, String target, String current) {
+//        Object currentShip = map.get(current);
+//        Object ship = map.get(target);
+//        if (ship == null && currentShip == null) {
+//            currentShip = new Object();
+//            map.put(current, currentShip);
+//            map.put(target, currentShip);
+//        } else if (currentShip == null) {
+//            map.put(current, ship);
+//        } else {
+//            map.put(target, currentShip);
+//        }
+//    }
 }
--- src/test/java/leet/next/_200_Number_of_IslandsTest.java
+++ src/test/java/leet/next/_200_Number_of_IslandsTest.java
@@ -1,5 +1,6 @@
 package leet.next;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,12 +12,31 @@ public class _200_Number_of_IslandsTest {
     @Test
     public void numIslands() throws Exception {
         _200_Number_of_Islands test = new _200_Number_of_Islands();
-        test.numIslands(new char[][]{
-                {1,1,1,1,0},
-                {1,1,0,1,0},
-                {1,1,0,0,0},
-                {0,0,0,0,0}
-        });
+        Assert.assertEquals(test.numIslands(new char[][]{
+                {'1','0','1','1','1'},
+                {'1','0','1','0','1'},
+                {'1','1','1','0','1'}
+        }), 1);
+
+        Assert.assertEquals(test.numIslands(new char[][]{
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        }), 1);
+
+        Assert.assertEquals(test.numIslands(new char[][]{
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        }), 3);
+
+        Assert.assertEquals(test.numIslands(new char[][]{
+                {'1','1','1'},
+                {'0','1','0'},
+                {'1','1','1'}
+        }), 1);
     }
 
 }
