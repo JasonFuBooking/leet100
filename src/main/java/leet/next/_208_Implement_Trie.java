b4b6b630a761515bf95a2fd35348940e2df40572
Jason Fu <jason.fu@booking.com>
2017-11-01 12:44:37 +0800

    208. Implement Trie (Prefix Tree)

--- src/main/java/leet/next/_208_Implement_Trie.java
+++ src/main/java/leet/next/_208_Implement_Trie.java
@@ -9,30 +9,64 @@ package leet.next;
  */
 
 /*
-实现一个trie啊。。。天哪真出这道题我就认了。
+实现一个trie啊。。。真的会有人出这道题么
 这里有详细解说： https://leetcode.com/problems/implement-trie-prefix-tree/solution/
+
+以下是抄的某高分答案，
+比较有意思的点：
+1。 insert里使用循环，逐个插入节点，并没有用递归
+2.  设置了一个isWord属性，这样搜索的时候可以方便的判断这个是不是一个词的结尾，同时要记得插入的时候设置上
+
  */
 public class _208_Implement_Trie {
 }
+class TrieNode {
+    public char val;
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+    public TrieNode() {}
+    TrieNode(char c){
+        TrieNode node = new TrieNode();
+        node.val = c;
+    }
+}
+
 class Trie {
-
-    /** Initialize your data structure here. */
+    private TrieNode root;
     public Trie() {
-
+        root = new TrieNode();
+        root.val = ' ';
     }
 
-    /** Inserts a word into the trie. */
     public void insert(String word) {
-
+        TrieNode ws = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(ws.children[c - 'a'] == null){
+                ws.children[c - 'a'] = new TrieNode(c);
+            }
+            ws = ws.children[c - 'a'];
+        }
+        ws.isWord = true;
     }
 
-    /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        return false;
+        TrieNode ws = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return ws.isWord;
     }
 
-    /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        return false;
+        TrieNode ws = root;
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return true;
     }
 }
