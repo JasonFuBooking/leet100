f4db72f2333768be6fe636d1c4c38ee8c6cff5e4
Jason Fu <jason.fu@booking.com>
2017-11-02 17:01:12 +0800

    621. Task Scheduler

--- src/main/java/leet/next/_621_Task_Scheduler.java
+++ src/main/java/leet/next/_621_Task_Scheduler.java
@@ -1,5 +1,7 @@
 package leet.next;
 
+import java.util.*;
+
 /**
  Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks.Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
 
@@ -15,8 +17,24 @@ package leet.next;
  The number of tasks is in the range [1, 10000].
  The integer n is in the range [0, 100].
  */
+
+/*
+请看第一个结题思路：
+https://leetcode.com/problems/task-scheduler/discuss/
+
+就是先按最数量最多的搭起框架，在逐个填充
+ */
 public class _621_Task_Scheduler {
     public int leastInterval(char[] tasks, int n) {
-        return 0;
+
+        int[] c = new int[26];
+        for(char t : tasks){
+            c[t - 'A']++;
+        }
+        Arrays.sort(c);
+        int i = 25;
+        while(i >= 0 && c[i] == c[25]) i--;
+
+        return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
     }
 }
---
+++ src/test/java/leet/next/_621_Task_SchedulerTest.java
@@ -0,0 +1,22 @@
+package leet.next;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jafu on 11/2/17.
+ */
+public class _621_Task_SchedulerTest {
+    @Test
+    public void leastInterval() throws Exception {
+        _621_Task_Scheduler test = new _621_Task_Scheduler();
+        Assert.assertEquals(test.leastInterval(new char[]{'A','A','A','B','B','B'}, 0), 6);
+        Assert.assertEquals(test.leastInterval(new char[]{'A','A','A','B','B','B'}, 2), 8);
+
+        Assert.assertEquals(test.leastInterval(new char[]{'A','A','C','C','C','D','D','E','E','E'}, 3), 10);
+        Assert.assertEquals(test.leastInterval(new char[]{'A','A','C','C','C','B','E','E','E'}, 2), 9);
+    }
+
+}
