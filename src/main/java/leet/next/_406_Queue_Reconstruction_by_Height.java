0af8c50b026a1a7aeb227c7ed00504158c8bea9c
Jason Fu <jason.fu@booking.com>
2017-11-02 10:59:43 +0800

    406. Queue Reconstruction by Height

--- src/main/java/leet/next/_406_Queue_Reconstruction_by_Height.java
+++ src/main/java/leet/next/_406_Queue_Reconstruction_by_Height.java
@@ -1,5 +1,7 @@
 package leet.next;
 
+import java.util.*;
+
 /**
  Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
 
@@ -14,41 +16,66 @@ package leet.next;
  Output:
  [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
  */
-public class _406_Queue_Reconstruction_by_Height {
-    public int[][] reconstructQueue(int[][] people) {
-        return null;
-    }
-}
 
 /*
-Java solution using Arrays.sort() and "insert sorting" idea
-We first sort the people to make them stand from the highest to shortest. For people with same height, sort them according to the count of people before them from small to big.
+这个完全是思路的问题。。。你要能想到方法，那就很简单
 
-Then, we use the way similar to insert sorting to reorder the people. For a given person to insert, all the people already sorted are higher, so we just insert him in the "right" place to make the people before him as his "count" indicates. Since he is shorter than all the people in the sorted list, the "count" of the "existing" people does not be broken by the insertion.
+首先按照高度排序。
+然后按照排序后的序列，一层一层按照k的插入新的位置。
+例子：
+先排序7开头的： 7,0 7,1
+接下来排6开头的，6只有1个，k值是1,把他插入到现有排好序的列表的1位置：
+【7，0】【6，1】 【7，1】
+为什么可以这样插？因为在6之前就只有7的元素，他们肯定是比6大的。
+按照k的定义，就是前边比6大的个数，那肯定都比他大呀，所以在都是7的列表里只要数个数就可以了
 
-    public int[][] reconstructQueue(int[][] people) {
-        if (people == null || people.length == 0 || people[0].length == 0)
-            return new int[0][0];
+后边的依次类推就可以
+ */
+public class _406_Queue_Reconstruction_by_Height {
+    class People {
+        Integer height;
+        Integer number;
 
-        Arrays.sort(people, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                if (b[0] == a[0]) return a[1] - b[1];
-                return b[0] - a[0];
-            }
-        });
-
-        int n = people.length;
-        ArrayList<int[]> tmp = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            tmp.add(people[i][1], new int[]{people[i][0], people[i][1]});
-
-        int[][] res = new int[people.length][2];
-        int i = 0;
-        for (int[] k : tmp) {
-            res[i][0] = k[0];
-            res[i++][1] = k[1];
+        public People(Integer height, Integer number) {
+            this.height = height;
+            this.number = number;
         }
 
-        return res;
+        @Override
+        public String toString() {
+            return "[" +
+                    "height=" + height +
+                    ", number=" + number +
+                    ']';
+        }
     }
- */
+    public int[][] reconstructQueue(int[][] people) {
+        List<People> list = new ArrayList<>();
+        for (int[] p : people) {
+            list.add(new People(p[0], p[1]));
+        }
+        TreeMap<Integer, TreeSet<People>> map = new TreeMap<>(Comparator.reverseOrder());
+        for (People p : list) {
+            TreeSet<People> set = map.get(p.height);
+            if (set == null) {
+                set = new TreeSet<>(Comparator.comparing(o -> o.number));
+            }
+            set.add(p);
+            map.put(p.height, set);
+        }
+        List<People> result = new ArrayList<>();
+        for (Integer key : map.keySet()) {
+            TreeSet<People> peoples = map.get(key);
+            for (People p : peoples) {
+                result.add(p.number, p);
+            }
+        }
+
+        for (int i = 0; i < people.length; ++i) {
+            people[i][0] = result.get(i).height;
+            people[i][1] = result.get(i).number;
+        }
+
+        return people;
+    }
+}
---
+++ src/test/java/leet/next/_406_Queue_Reconstruction_by_HeightTest.java
@@ -0,0 +1,17 @@
+package leet.next;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jafu on 11/2/17.
+ */
+public class _406_Queue_Reconstruction_by_HeightTest {
+    @Test
+    public void reconstructQueue() throws Exception {
+        _406_Queue_Reconstruction_by_Height test = new _406_Queue_Reconstruction_by_Height();
+        test.reconstructQueue(new int[][]{{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}});
+    }
+
+}
